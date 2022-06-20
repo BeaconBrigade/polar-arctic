7c69aa79d214fd681618e8fcd9a8db48ae72f596
BeaconBrigade <BeaconBrigade@gmail.com>
2022-06-20 18:35:07 -0230

    Added struct for metadata and fixed gui

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use csv::ReaderBuilder;
 use iced::{
     alignment, executor, text_input, Application, Column, Command, Element, Length, Subscription,
-    Text, TextInput,
+    Text, TextInput, Row, Rule,
 };
 use plotters::prelude::*;
 use plotters_iced::{Chart, ChartWidget, DrawingBackend};
@@ -15,7 +15,8 @@ pub struct Menu {
     device_input: text_input::State,
     device_id: String,
     sensor: Option<()>, // TODO - fill with actual arctic sensor when update is made
-    ecg: EcgChart,
+    chart: EcgChart,
+    meta: Option<Meta>,
 }
 
 #[derive(Debug, Clone)]
@@ -24,6 +25,7 @@ pub enum Message {
     Tick,
     InputChanged(String),
     CreateSensor,
+    NewMeta,
 }
 
 impl Application for Menu {
@@ -32,10 +34,15 @@ impl Application for Menu {
     type Flags = ();
 
     fn new(_flags: ()) -> (Self, Command<Message>) {
-        let mut menu = Menu::default();
-        menu.ecg.init_data().expect("Error loading data");
+        let chart = EcgChart::new().expect("Error making graph.");
 
-        (menu, Command::none())
+        (
+            Menu {
+                chart,
+                ..Default::default()
+            },
+            Command::none(),
+        )
     }
 
     fn title(&self) -> String {
@@ -53,6 +60,7 @@ impl Application for Menu {
                 // Construct sensor
                 println!("Sensor created!");
             }
+            Message::NewMeta => {}
         }
 
         Command::none()
@@ -80,24 +88,90 @@ impl Application for Menu {
 
         let testing = Text::new(&self.device_id).size(25).width(Length::Fill);
 
-        Column::new()
+        // Data side
+        let left = Column::new()
             .spacing(20)
             .width(Length::Fill)
             .max_width(1000)
-            .push(title)
             .push(input)
             .push(testing)
-            .push(self.ecg.view())
+            .push(self.chart.view());
+
+
+        let meta_header = Text::new("Meta")
+            .width(Length::Fill)
+            .size(40);
+
+        // meta side
+        let right = Column::new()
+            .spacing(20)
+            .width(Length::Fill)
+            .max_width(1000)
+            .push(meta_header);
+
+
+        // full view
+        let body = Row::new()
+            .spacing(20)
+            .width(Length::Fill)
+            .height(Length::Fill)
+            .push(left)
+            .push(Rule::vertical(2))
+            .push(right);
+
+        Column::new()
+            .push(title)
+            .push(body)
             .into()
     }
 }
 
+// Store chart data
 #[derive(Default)]
-pub struct EcgChart {
+struct EcgChart {
     data_points: VecDeque<(u64, f32)>,
 }
 
+impl EcgChart {
+    pub fn new() -> Result<EcgChart, Box<dyn std::error::Error>> {
+        let mut chart = Self::default();
+        chart.init_data()?;
+
+        Ok(chart)
+    }
+
+    // Draw chart
+    fn view(&mut self) -> Element<Message> {
+        let chart = ChartWidget::new(self)
+            .width(Length::Units(400))
+            .height(Length::Units(400));
+
+        chart.into()
+    }
+
+    // Get initial data from file
+    fn init_data(&mut self) -> Result<(), Box<dyn std::error::Error>> {
+        let mut rdr = ReaderBuilder::new()
+            .flexible(true)
+            .from_path("output/test.csv")
+            .unwrap();
+
+        // skip extra header row
+        for record in rdr.records().skip(1) {
+            let result = record?;
+            self.data_points
+                .push_back((result[0].parse()?, result[1].parse::<f32>()? * 100.0));
+        }
+
+        Ok(())
+    }
+
+    // Update data - remove data from the end and add to the start
+    fn update(&mut self) {}
+}
+
 impl Chart<Message> for EcgChart {
+    // Create plotters chart
     fn build_chart<DB: DrawingBackend>(&self, mut builder: ChartBuilder<DB>) {
         let start = self.data_points[0].0;
         let end = self.data_points.back().unwrap().0;
@@ -122,27 +196,36 @@ impl Chart<Message> for EcgChart {
     }
 }
 
-impl EcgChart {
-    pub fn new() -> EcgChart {
-        EcgChart::default()
-    }
+// Store meta-data about this run
+struct Meta {
+    pub first_name: String,
+    pub last_name: String,
+    pub group_number: i8,
+}
 
-    fn view(&mut self) -> Element<Message> {
-        let chart = ChartWidget::new(self)
-            .width(Length::Units(400))
-            .height(Length::Units(400));
+// Store errors converting input to meta data
+enum ParseError {
+    First,
+    Last,
+    GroupNumber,
+}
 
-        chart.into()
-    }
+impl TryFrom<[&str; 3]> for Meta {
+    type Error = ParseError;
 
-    fn init_data(&mut self) -> Result<(), Box<dyn std::error::Error>> {
-        let mut rdr = ReaderBuilder::new().from_path("output/test.csv").unwrap();
-
-        for record in rdr.records() {
-            let result = record?;
-            self.data_points.push_back((result[0].parse()?, result[1].parse::<f32>()? * 100.0));
-        }
-
-        Ok(())
+    fn try_from(data: [&str; 3]) -> Result<Self, Self::Error> {
+        Ok(Self {
+            first_name: if !data[0].is_empty() {
+                data[0].to_owned()
+            } else {
+                return Err(ParseError::First);
+            },
+            last_name: if !data[1].is_empty() {
+                data[1].to_owned()
+            } else {
+                return Err(ParseError::Last);
+            },
+            group_number: data[2].parse().map_err(|_| ParseError::GroupNumber)?,
+        })
     }
 }
